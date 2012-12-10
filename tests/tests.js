cb21df9dba784ca31e4b098eea47b299cd86cb81
Thiago Avelino <thiagoavelinoster@gmail.com>
2012-12-10 21:24:26 -0200

    Add Test two insert in Test API insert

--- tests/tests.js
+++ tests/tests.js
@@ -10,9 +10,23 @@ Object.prototype.getKey = function(value){
 module("Test API")
 test( "Test one insert", function() {
   localStorage.clear();
+
   one_insert = zon('lerolero').insert('test one insert');
   ok(one_insert, "Test one insert");
   equal(1, localStorage.length);
   equal("lerolero|" + one_insert, localStorage.getKey('test one insert'));
+});
+test( "Test two insert", function() {
+  localStorage.clear();
+
+  one_insert = zon('lerolero').insert('test one insert');
+  two_insert = zon('lerolero').insert('test two insert');
+
+  ok(one_insert, "Test one insert");
+  ok(two_insert, "Test two insert");
+
+  equal(2, localStorage.length);
+  equal("lerolero|" + one_insert, localStorage.getKey('test one insert'));
+  equal("lerolero|" + two_insert, localStorage.getKey('test two insert'));
 });
 
